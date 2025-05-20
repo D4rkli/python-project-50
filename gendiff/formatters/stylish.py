ec7ca0582f5f16d44a15936d52706c3c9a93e12d
Anzhelika <darkl11n3ss@gmail.com>
2025-05-21 01:25:51 +0300

    Fix tests

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -9,35 +9,35 @@ def format_stylish(diff, depth=0):
                           f"{format_stylish(node['children'], depth + 1)}")
         elif node["type"] == "added":
             result.append(f"{indent}  + {key}: "
-                          f"{format_value(node['value'], depth + 1)}")
+                          f"{to_str(node['value'], depth + 1)}")
         elif node["type"] == "removed":
             result.append(f"{indent}  - {key}: "
-                          f"{format_value(node['value'], depth + 1)}")
+                          f"{to_str(node['value'], depth + 1)}")
         elif node["type"] == "changed":
             result.append(f"{indent}  - {key}: "
-                          f"{format_value(node['old_value'], depth + 1)}")
+                          f"{to_str(node['old_value'], depth + 1)}")
             result.append(f"{indent}  + {key}: "
-                          f"{format_value(node['new_value'], depth + 1)}")
+                          f"{to_str(node['new_value'], depth + 1)}")
         else:
             result.append(f"{indent}    {key}: "
-                          f"{format_value(node['value'], depth + 1)}")
+                          f"{to_str(node['value'], depth + 1)}")
 
     result.append(indent + "}")
     return "\n".join(result)
 
 
-def format_value(value, depth):
+def to_str(value, depth):
     if value is None:
         return "null"
 
     if isinstance(value, bool):
-        return "true" if value else "false"
+        return str(value).lower()
 
     if isinstance(value, dict):
         indent = " " * ((depth + 1) * 4)
         formatted = ["{"]
         for k, v in value.items():
-            formatted.append(f"{indent}{k}: {format_value(v, depth + 1)}")
+            formatted.append(f"{indent}{k}: {to_str(v, depth + 1)}")
         formatted.append(" " * (depth * 4) + "}")
         return "\n".join(formatted)
 
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,16 +3,24 @@ import os
 import yaml
 
 
-def parse_file(filepath):
+def read_file(filepath):
     """Определяет формат файла и парсит его."""
     if not os.path.exists(filepath):
         raise FileNotFoundError(f"Файл '{filepath}' не найден.")
 
+    with open(filepath, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+def parse_content(content:str, filepath: str):
     if filepath.endswith((".json", ".JSON")):
-        with open(filepath, "r", encoding="utf-8") as file:
-            return json.load(file)
+        return json.loads(content)
     elif filepath.endswith((".yml", ".yaml", ".YML", ".YAML")):
-        with open(filepath, "r", encoding="utf-8") as file:
-            return yaml.safe_load(file)
+        return yaml.safe_load(content)
     else:
         raise ValueError(f"Неподдерживаемый формат файла: {filepath}")
+
+
+def parse_file(filepath):
+    content = read_file(filepath)
+    return parse_content(content, filepath)
